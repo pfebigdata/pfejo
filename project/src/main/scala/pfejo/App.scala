9fcc369dcd74cb68d4bdcdc1d218389b7dee3835
Mohamed.ZAFAD <dev@zafad.org>
2019-07-14 17:17:25 +0200

    1

--- project/src/main/scala/pfejo/App.scala
+++ project/src/main/scala/pfejo/App.scala
@@ -1,34 +1,18 @@
 package pfejo
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-git
+import org.apache.spark.{SparkConf, SparkContext}
 
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
+
   def main(args : Array[String]) {
-    println( "Hello pfejo!" )
-    println("concat arguments = " + foo(args))
-
-    val sc = new SparkContext("local", "Word Count (2)", new SparkConf())
-
-    try {
-
-
-      val out = "output/kjv-wc2"
-      FileUtil.rmrf(out)
-      val input = sc.textFile("data/kjvdat.txt").map(line => line.toLowerCase)
-      val wc = input
-        .flatMap(line => line.split("""[^\p{IsAlphabetic}]+"""))
-        .map(word => (word, 1))
-        .reduceByKey((count1, count2) => count1 + count2)
-      println(s"Writing output to: $out")
-      wc.saveAsTextFile(out)
-    } finally {
-      sc.stop()      // Stop (shut down) the context.
-    }
+    println( "pfejo starting!" )
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("spark_pfe_jo")
+    val sc = new SparkContext(conf)
+    val rdd1 = sc.makeRDD(Array(1,2,3,4,5))
+    rdd1.collect().foreach(println)
   }
 
 }
