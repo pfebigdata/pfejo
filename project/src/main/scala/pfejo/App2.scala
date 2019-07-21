d77a596f086251c521eed30b86d44e35e301fca4
mzafad <dev@zafad.org>
2019-07-21 10:49:54 +0200

    1

--- project/src/main/scala/pfejo/App2.scala
+++ project/src/main/scala/pfejo/App2.scala
@@ -1,27 +1,46 @@
-package pfejo
+
+
+
+package com.premierScala.spark
+
+import org.apache.spark._
+import org.apache.spark.sql._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions.{col, lit, when,sum,desc,rank}
-object App2 {
-  def main(args : Array[String]) {
-    println( "pfejo starting!" )
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.{col, lit, when,max,sum,desc,rank}
+
+object PremierScala {
+
+  def main(args:Array[String]) : Unit ={
     val spark = SparkSession.builder().appName("PremiereClass").master("local").getOrCreate()
+
     val co = spark.read.format("csv").
       option("delimiter",",").
       option("escape","\"").
       option("nullValue","NA").
       load("hdfs:///user/hdfsjo/brut.csv").
       toDF("ID","Name","Sex","Age","Height","Weight","Team","NOC","Games","Year","Season","City","Sport","Event","Medal")
-    co.select("NOC","ID","Name","Medal").
-      where("Medal is not null").
+
+
+    val theLastYear = co.groupBy().agg(max("Year")).first().getString(0)
+
+    val req1 = co.select( "Year","ID","Name", "SEX", "Medal").
+      withColumn("lastYear", lit(theLastYear)).
+      where("year = lastYear and Medal is not null").
       withColumn("nbGold", when(col("Medal")=== "Gold",1)).
       withColumn("nbSilver", when(col("Medal")=== "Silver",1)).
       withColumn("nbBronze", when(col("Medal")=== "Bronze",1)).
-      groupBy("NOC","ID","Name").
+      groupBy( "Year","ID","Name", "SEX").
       agg(sum("nbGold").alias("sumGold"), sum("nbSilver").alias("sumSilver"),sum("nbBronze").alias("sumBronze")).
-      withColumn("e", rank().over(   Window.partitionBy("NOC").orderBy(col("sumGold").desc , col("sumSilver").desc , col("sumBronze").desc))).
-      //orderBy("NOC", col("sumGold").desc , col("sumSilver").desc , col("sumBronze").desc).
-      where (" e <=5 ").
-      show()
+      withColumn("e", rank().over(   Window.partitionBy("SEX").orderBy("sumGold desc" , "sumSilver desc", "sumBronze desc")    )).
+      where (" e <=5 ")
+
+
+
   }
+
+
 }
